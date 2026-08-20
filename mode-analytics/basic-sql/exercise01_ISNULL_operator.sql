88cef1b6568cf0e607feb4bffdee35c25f060f90
Vinicius <viny.fsm@gmail.com>
2026-08-20 12:13:16 -0300

    Add first basic-sql exercise query

---
+++ mode-analytics/basic-sql/exercise01_ISNULL_operator.sql
@@ -0,0 +1,11 @@
+-- ===================================================================
+-- Course: ThoughtSpot / Mode Analytics Tutorial
+-- Chapter: Basic SQL 
+-- Available Tables: tutorial_billboard_top_100_year_end
+-- Exercise Goals: Get used to the ISNULL operator.
+--                 Identify tracks that are missing a song title in the database.
+-- ===================================================================
+
+SELECT *
+FROM tutorial_billboard_top_100_year_end
+WHERE song_name IS NULL OR TRIM(song_name) = '';
