d03d719ee954986edfb36af79c8fecf9e018e8cd
Vinicius <viny.fsm@gmail.com>
2026-09-04 10:11:42 -0300

    Added exercise 3

---
+++ exercise03_OR_operator.sql
@@ -0,0 +1,25 @@
+-- ===================================================================
+-- Course: ThoughtSpot / Mode Analytics Tutorial
+-- Chapter: Basic SQL 
+-- Available Tables: tutorial.billboard_top_100_year_end
+-- Exercises Goals: Get used to the OR operator.
+-- ===================================================================
+
+
+-- Write a query that returns all rows for top-10 songs that featured either Katy Perry or Bon Jovi:
+SELECT *   
+  FROM tutorial.billboard_top_100_year_end  
+WHERE year_rank <= 10
+  AND ("group_name" ILIKE '%katy perry%' OR "group_name" ILIKE '%bon jovi%');
+
+-- Write a query that returns all songs with titles that contain the word "California" in either the 1970s or 1990s.
+SELECT *   
+  FROM tutorial.billboard_top_100_year_end  
+WHERE song_name ILIKE '%california%'
+  AND (year BETWEEN 1970 AND 1979 OR year BETWEEN 1990 and 1999)
+
+-- Write a query that lists all top-100 recordings that feature Dr. Dre before 2001 or after 2009.
+SELECT *   
+  FROM tutorial.billboard_top_100_year_end  
+WHERE "group_name" ILIKE '%dr. dre%'
+  AND (year < 2001 OR year > 2009)
