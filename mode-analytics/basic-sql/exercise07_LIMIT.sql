c65dcdcbaa94c539104137169f1ef11c91ccff08
Vinicius <viny.fsm@gmail.com>
2026-09-21 14:23:13 -0300

    Added exercise 08

---
+++ mode-analytics/basic-sql/exercise07_LIMIT.sql
@@ -0,0 +1,11 @@
+-- ===================================================================
+-- Course: ThoughtSpot / Mode Analytics Tutorial
+-- Chapter: Basic SQL 
+-- Available Tables: tutorial.us_housing_units
+-- Exercises Goals: Get used to the LIMIT statement.
+-- ===================================================================
+
+-- Write a query that uses the LIMIT command to restrict the result set to only 15 rows.
+SELECT *
+  FROM tutorial.us_housing_units
+ LIMIT 15;
